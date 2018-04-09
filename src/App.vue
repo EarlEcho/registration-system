cab46ff32317e09ef5eed01980724d4e7254617c
EarlEcho <734952325@qq.com>
2018-04-09 20:57:23 +0800

    头部组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
     <div id="app">
-        <div v-if="showHeader" class="user-header-infos">
-            <span>{{userInfos.username}}</span>&emsp;欢迎登录&emsp;&emsp;
-            <el-button type="text">
-                <i class="icon ion-power"></i>&nbsp;&nbsp;注销登录&emsp;&emsp;
-            </el-button>
-        </div>
         <router-view/>
     </div>
 </template>
 
 <script>
+
     import functions from '@/functions/common.js'
 
     export default {
@@ -19,7 +14,6 @@
         props: [],
         data() {
             return {
-                showHeader: false,
                 userInfos: {
                     id: 1,
                     username: 'Echo'
@@ -27,17 +21,17 @@
             }
         },
         mounted() {
-            if (window.location.hash == '#/sing-up' || window.location.hash == '#/sing-in') {
+            /*if (window.location.hash == '#/sing-up' || window.location.hash == '#/sing-in') {
                 this.showHeader = false;
             } else {
 
-                /*获取当前登录用户的ID*/
+                /!*获取当前登录用户的ID*!/
                 // let userId = this.$route.query.id;
                 functions.getAjax('/regs/user/getOne', (res) => {
                     this.userInfos = res.data;
                     this.showHeader = true;
                 });
-            }
+            }*/
         }
     }
 </script>
@@ -53,15 +47,6 @@
         background-color: #f5f5f5;
     }
 
-    .user-header-infos {
-        font-size: 14px;
-        width: 100%;
-        background: #409EFF;
-        text-align: right;
-        span {
-            color: white;
-        }
-    }
 
     .module-content-w {
         width: 85%;
